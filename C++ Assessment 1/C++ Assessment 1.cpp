fd507682e4fcc18d409acca1dfef89811f6ed7ba
KevinBacklund <kebacklund@gmail.com>
2026-02-25 21:40:51 +0100

    Added minigame and basic combat

--- C++ Assessment 1/C++ Assessment 1.cpp
+++ C++ Assessment 1/C++ Assessment 1.cpp
@@ -10,17 +10,28 @@ struct Position
 	int y;
 };
 
+struct Enemy
+{
+	std::string name;
+	std::string description;
+	int health;
+	int attackPower;
+};
+
 struct Room
 {
 	std::string name;
 	std::string description;
 	std::vector<std::string> items;
+	std::vector<Enemy> enemies;
 };
 
 struct Player
 {
 	Position position;
 	std::vector<std::string> inventory;
+	int health;
+	int attackPower;
 };
 
 void Quit()
@@ -49,12 +60,17 @@ void Commands()
 	std::cout << "5. MOVE <DIRECTION> - Move in a specified direction (NORTH, SOUTH, EAST, WEST)\n";
 	std::cout << "6. PICKUP <ITEM> - Pick up an item in the current room\n";
 	std::cout << "7. INVENTORY - Show your inventory\n";
+	std::cout << "8. USE <ITEM> - Use an item from your inventory or the current room\n";
+	std::cout << "9. LOOK - Look around the current room\n";
+	std::cout << "10. ATTACK <ENEMY> - Attack an enemy in the current room\n";
 }
 
 void Help() 
 {
 	std::cout << "Help Information:\n";
 }
+
+
 
 void ShowInventory(Player player) 
 {
@@ -74,14 +90,43 @@ void ShowInventory(Player player)
 	}
 }
 
+bool Contains(std::vector<std::string> items, std::string item) 
+{
+	for (std::string itm : items) 
+	{
+		if (itm == item) 
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 void ShowRoomDescription(Position roomPos, std::vector<std::vector<Room>>& map) 
 {
-	std::cout << map[roomPos.x][roomPos.y].description + "\n";
-	std::cout << "Items in the room:\n";
-	for (std::string& item : map[roomPos.x][roomPos.y].items) 
+	Room room = map[roomPos.x][roomPos.y];
+	std::cout << room.description + "\n";
+	if (!room.items.empty())
 	{
-		std::cout << "- " + item + "\n";
+		std::cout << "Items in the room:\n";
+		for (std::string& item : room.items)
+		{
+			std::cout << "- " + item + "\n";
+		}
 	}
+	if (!room.enemies.empty())
+	{
+		std::cout << "Enemies in the room:\n";
+		for (Enemy& enemy : room.enemies) 
+		{
+			std::cout << "- " + enemy.name + ": " + enemy.description + "\n";
+		}
+	}
+}
+
+void Look(Player& player, std::vector<std::vector<Room>>& map) 
+{
+	ShowRoomDescription(player.position, map);
 }
 
 void Move(Position& playerPos, std::string direction, std::vector<std::vector<Room>>& map)
@@ -122,6 +167,134 @@ void PickUp(std::string item, Player& player, std::vector<std::vector<Room>>& ma
 	else 
 	{
 		std::cout << "You could not pick up" + item + "\n";
+	}
+}
+
+void UseMap(Player& player, std::vector<std::vector<Room>>& map)
+{
+	std::cout << "You used the map. Here is the layout of the rooms:\n";
+	for (int i = 0; i < map.size(); ++i) 
+	{
+		for (int j = 0; j < map[i].size(); ++j) 
+		{
+			std::cout << "[" << map[i][j].name << "]";
+			if (player.position.x == i && player.position.y == j) 
+			{
+				std::cout << " <- You are here";
+			}
+			std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
+void GuessTheWord() 
+{
+	std::string possibleWords[]  = { "HELLO", "WORLD", "GAME", "PROGRAMMING", "ADVENTURE" };
+	std::string wordToGuess = possibleWords[rand() % possibleWords->length()];
+	std::string playerGuess;
+	int wordLength = wordToGuess.length();
+	std::cout << "Welcome to Guess the Word! Try to guess the word.\nThe word has " << wordLength << " letters." << std::endl;
+	while (playerGuess != wordToGuess) 
+	{
+		std::cout << "Enter your guess: ";
+		std::getline(std::cin, playerGuess);
+		std::transform(playerGuess.begin(), playerGuess.end(), playerGuess.begin(), ::toupper);
+		if (playerGuess == wordToGuess) 
+		{
+			std::cout << "Congratulations! You guessed the word!\n";
+		}
+		else if (playerGuess == "QUIT") 
+		{
+			Quit();
+		}
+		else 
+		{
+			int correctLetters = 0;
+			for (int i = 0; i < wordLength; i++) 
+			{
+				for (int j = 0; j < playerGuess.length(); j++)
+				{
+					if (wordToGuess[i] == playerGuess[j]) 
+					{
+						correctLetters++;
+						break;
+					}
+				}
+			}
+			std::cout << "You guessed " << correctLetters << " letters correctly." << std::endl;
+		}
+	}
+}
+
+void UseItem(std::string item, Player& player, std::vector<std::vector<Room>>& map)
+{
+	Room& currentRoom = map[player.position.x][player.position.y];
+	auto foundInventoryItem = std::find(player.inventory.begin(), player.inventory.end(), item);
+	auto foundRoomItem = std::find(currentRoom.items.begin(), currentRoom.items.end(), item);
+	if ((foundInventoryItem == player.inventory.end()) && (foundRoomItem == currentRoom.items.end()))
+	{
+		std::cout << "You don't have " + item + "\n";
+		return;
+	}
+	if (foundRoomItem != currentRoom.items.end()) 
+	{
+		PickUp(item, player, map);
+	}
+	if (item == "MAP") 
+	{
+		UseMap(player, map);
+	}
+	else if (item == "GAMECONSOLE") 
+	{
+		GuessTheWord();
+	}
+	else 
+	{
+		std::cout << "You can't use " + item + " right now\n";
+	}
+}
+
+void AttackEnemy(std::string& enemyName, Player& player, std::vector<std::vector<Room>>& map)
+{
+	Room& currentRoom = map[player.position.x][player.position.y];
+	int enemyIndex = -1;
+	int attackPower = player.attackPower;
+	for ( int i = 0; i < currentRoom.enemies.size(); i++) 
+	{
+		if (currentRoom.enemies[i].name == enemyName)
+		{
+			enemyIndex = i;
+			break;
+		}
+	}
+
+	Enemy& enemy = currentRoom.enemies[enemyIndex];
+
+	if (enemyIndex < 0) 
+	{
+		std::cout << "You can't find a " + enemyName + " to attack\n";
+		return;
+	}
+
+	if (Contains(player.inventory, "SWORD")) 
+	{
+		attackPower += 5;
+		std::cout << "You slash the " + enemy.name + " with your sword for " << attackPower << " damage.\n";
+	}
+	else
+	{
+		std::cout << "You punch the " + enemy.name + " for " << attackPower << " damage.\n";
+	}
+	enemy.health -= attackPower;
+	if (enemy.health <= 0) 
+	{
+		std::cout << "You defeated the " + enemy.name + "!\n";
+		currentRoom.enemies.erase(currentRoom.enemies.begin() + enemyIndex);
+	}
+	else 
+	{
+		std::cout << "The " + enemy.name + " has " << enemy.health << " health remaining.\n";
 	}
 }
 
@@ -186,6 +359,35 @@ void ProcessCommand(std::string& command, std::string& commandLog, Player& playe
 		AddToLog(commandLog, command);
 		ShowInventory(player);
 	}
+	else if (words[0] == "USE")
+	{
+		if (words.size() > 1)
+		{
+			AddToLog(commandLog, command);
+			UseItem(words[1], player, map);
+		}
+		else
+		{
+			std::cout << "Please specify an item to use.\n";
+		}
+	}
+	else if (words[0] == "LOOK")
+	{
+		AddToLog(commandLog, command);
+		Look(player, map);
+	}
+	else if (words[0] == "ATTACK")
+	{
+		if (words.size() > 1)
+		{
+			AddToLog(commandLog, command);
+			AttackEnemy(words[1], player, map);
+		}
+		else
+		{
+			std::cout << "Please specify an enemy to attack\n";
+		}
+	}
 	else
 	{
 		std::cout << "Unknown command: " << words[0] << "\n";
@@ -206,15 +408,19 @@ int main()
 	std::string commandLog;
 	Player player;
 	player.position = { 0, 0 };
+	player.health = 100;
+	player.attackPower = 10;
+
+	Enemy goblin = { "GOBLIN", "A small, green creature with sharp teeth.", 30, 5 };
 	std::vector<std::vector<Room>> map = 
 	{
 		{
-			{"Room 1", "This is the first room.", {"KEY", "MAP"}},
+			{"Room 1", "This is the first room.", {"KEY", "MAP", "GAMECONSOLE"}},
 			{"Room 2", "This is the second room.", {"SWORD", "SHIELD"}}
 		},
 		{
 			{"Room 3", "This is the third room.", {"POTION", "SCROLL"}},
-			{"Room 4", "This is the fourth room.", {"GEM", "COIN"}}
+			{"Room 4", "This is the fourth room.", {"GEM", "COIN"}, {goblin}}
 		}
 	};
 	ShowRoomDescription(player.position, map);
